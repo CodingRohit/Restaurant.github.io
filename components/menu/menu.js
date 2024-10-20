c6ee810836d684e54ba1b7f9f99da933d74e3166
Rohit <rkhero92@gmail.com>
2024-10-20 19:31:24 +0530

    Restaurant website
    It is a online food order website

---
+++ components/menu/menu.js
@@ -0,0 +1,4 @@
+function toggleDescription(itemId) {
+    const description = document.getElementById(itemId);
+    description.style.display = description.style.display === 'block' ? 'none' : 'block';
+}
